c593eb022e867d15681eb9a7b0fb60f62367857e
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-05 17:51:42 +0900

    feat: 몬스터 스탯

---
+++ Assets/02.Scripts/Monster/MonsterStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MonsterStats : MonoBehaviour
+{
+    [SerializeField] private ValueStat _moveSpeed;
+
+    public float Speed => _moveSpeed.Value;
+
+    private void Start()
+    {
+        InitializeStats();
+    }
+
+    private void InitializeStats()
+    {
+        _moveSpeed.Initialize();
+    }
+}
