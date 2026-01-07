bbcd702e61e8a0c31f5d4c5e1b6312ee103f3479
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-07 13:16:56 +0900

    feat: 몬스터 스탯 정의

---
+++ Assets/02.Scripts/Monster/Stat/MonsterStat.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System;
+
+public class MonsterStat : MonoBehaviour
+{
+    public ConsumableStat Heatlh;
+    public ValueStat Damage;
+    public ValueStat MoveSpeed;
+}
---
+++ Assets/02.Scripts/Monster/UI/MonsterHealthBar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(MonsterStat))]
+public class MonsterHealthBar : MonoBehaviour
+{
+    [Header("체력 게이지")]
+    [SerializeField] private Transform _healthBarTransform;
+    [SerializeField] private Image _gaugeImage;
+
+    private MonsterStat _stat;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _stat = GetComponent<MonsterStat>();
+        _camera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        _stat.Heatlh.OnValueChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _stat.Heatlh.OnValueChanged -= OnHealthChanged;
+    }
+
+    private void LateUpdate()
+    {
+        _healthBarTransform.forward = _camera.transform.forward;
+    }
+
+    private void OnHealthChanged(float current, float max)
+    {
+        _gaugeImage.fillAmount = current / max;
+    }
+}
