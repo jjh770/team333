619f6e11595e3704e346efd45cc044f6b84c765f
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-07 18:15:59 +0900

    feat: monster pool

---
+++ Assets/02.Scripts/Monster/Core/Monster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour, IPoolable
+{
+    private MonsterStat _stat;
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _stat = GetComponent<MonsterStat>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    public void OnSpawn()
+    {
+        if (_agent == null) return;
+        _agent.enabled = true;
+    }
+
+    public void OnDespawn()
+    {
+        if (_agent == null) return;
+        _agent.enabled = false;
+    }
+
+    public void Die()
+    {
+        PoolManager.Instance.Return(gameObject);
+    }
+}
---
+++ Assets/02.Scripts/Monster/Spawn/MonsterPool.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MonsterPool : MonoBehaviour
+{
+    [SerializeField] private GameObject _monsterPrefab;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private int _preloadCount = 5;
+
+    private void Start()
+    {
+        if (_monsterPrefab != null)
+        {
+            PoolManager.Instance.Preload(_monsterPrefab, _preloadCount);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Spawn();
+        }
+    }
+
+    public GameObject Spawn()
+    {
+        Vector3 position = _spawnPoint != null ? _spawnPoint.position : transform.position;
+        Quaternion rotation = _spawnPoint != null ? _spawnPoint.rotation : Quaternion.identity;
+
+        return PoolManager.Instance.Get(_monsterPrefab, position, rotation);
+    }
+}
