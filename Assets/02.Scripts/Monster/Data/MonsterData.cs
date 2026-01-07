256829e8471ed654cb686cf70087a9ca1bd9db5c
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-07 14:08:48 +0900

    chore: 씬 저장

---
+++ Assets/02.Scripts/Monster/Data/MonsterData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Monster/MonsterData")]
+public class MonsterData : ScriptableObject
+{
+    [Header("Identity")]
+    [SerializeField] private string _monsterName;
+}
