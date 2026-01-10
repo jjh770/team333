bd2da90c8a4a001363f6e2d6bc084312b4874602
huiyeon <paige4157@gmail.com>
2026-01-10 22:29:47 +0900

    feat: 말풍선 두트윈

--- Assets/02.Scripts/UI/UI_MonsterSpeechBubble.cs
+++ Assets/02.Scripts/UI/UI_MonsterSpeechBubble.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class UI_MonsterSpeechBubble : MonoBehaviour
 {
+    [Header("SpeechBubble")]
     [SerializeField] private GameObject _speechBubble;
     [SerializeField] private Image _iconImage;
 
@@ -12,12 +13,32 @@ public class UI_MonsterSpeechBubble : MonoBehaviour
     [SerializeField] private Sprite _happySprite;
     private Sprite _currrentSprite;
 
-    [Header("Animation")]
+    [Header("Icon Animation")]
     [SerializeField] private float _fadeDuration = 0.12f;
     [SerializeField] private float _popScale = 1.08f;
     [SerializeField] private float _popDuration = 0.12f;
 
-    private Tweener _tween;
+    [Header("Floating Animation")]
+    [SerializeField] private float _floatDistance = 0.1f;
+    [SerializeField] private float _floatDuration = 1.5f;
+
+    private Tween _floatTween;
+    private Tweener _iconTween;
+
+    private void Start()
+    {
+        StartFloating();
+    }
+
+    private void StartFloating()
+    {
+        
+        RectTransform bubbleRect = _speechBubble.GetComponent<RectTransform>();
+
+        _floatTween = bubbleRect.DOAnchorPosY(bubbleRect.anchoredPosition.y + _floatDistance, _floatDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
 
     public void SetSad()
     {
@@ -33,5 +54,22 @@ public class UI_MonsterSpeechBubble : MonoBehaviour
     {
         _currrentSprite = sprite;
         _iconImage.sprite = _currrentSprite;
+
+        PlayPopAnimation();
+    }
+
+    private void PlayPopAnimation()
+    {
+        _iconTween?.Kill(true);
+
+        _iconImage.transform.localScale = Vector3.one;
+        _iconTween = _iconImage.transform.DOScale(_popScale, _popDuration)
+            .SetLoops(2, LoopType.Yoyo);
+    }
+
+    private void OnDestroy()
+    {
+        _floatTween?.Kill();
+        _iconTween?.Kill();
     }
 }
