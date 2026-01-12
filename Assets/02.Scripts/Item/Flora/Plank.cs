abc51c3e2db90258a55e5faf80aa8ba354de6751
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-01-12 16:10:12 +0900

    feat: 판자 퀘스트 구현

---
+++ Assets/02.Scripts/Item/Flora/Plank.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Plank : ItemBase
+{
+    private const string QuestBridgeTag = "QuestBridge";
+    private ItemFactory _itemFactory;
+
+    override protected void Awake()
+    {
+        base.Awake();
+        _itemFactory = ItemFactory.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isHeld) return;
+        if (!other.CompareTag(QuestBridgeTag)) return;
+
+        BridgeQuestManager bridgeQuestManager = other.GetComponentInParent<BridgeQuestManager>();
+        if (bridgeQuestManager != null)
+        {
+            bridgeQuestManager.AddPlank();
+        }
+
+        _itemFactory.Despawn(this.gameObject);
+    }
+}
---
+++ Assets/02.Scripts/Manager/BridgeQuestManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class BridgeQuestManager : MonoBehaviour
+{
+    [SerializeField] private int _needBoardCount = 9;
+    [SerializeField] private GameObject[] _plankObjects;
+    [SerializeField] private PlankMonsterSpawner _plankMonsterSpawner;
+    
+    private int _boardCount = 0;
+    public bool IsQuestCompleted { get; private set; }
+
+    private void Start()
+    {
+        foreach (GameObject obj in _plankObjects)
+        {
+            obj.SetActive(false);
+        }
+    }
+    public void AddPlank()
+    {
+        if (IsQuestCompleted)
+        {
+            return;
+        }
+        
+        _plankObjects[_boardCount].SetActive(true);
+        _boardCount++;
+
+        if (_boardCount == _needBoardCount)
+        {
+            CompleteQuest();
+        }
+    }
+
+    private void CompleteQuest()
+    {
+        IsQuestCompleted = true;
+        Debug.Log($"미션 완료!");
+        
+        _plankMonsterSpawner.StopSpawning();
+    }
+}
