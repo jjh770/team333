a8e0cc990e04e96172ff309ce3539ab4d2100bbd
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-12 12:56:11 +0900

    chore: 아이템을 스포너 하위로

--- Assets/02.Scripts/Monster/Components/MonsterItemDropComponent.cs
+++ Assets/02.Scripts/Monster/Components/MonsterItemDropComponent.cs
@@ -4,11 +4,18 @@ public class MonsterItemDropComponent : MonoBehaviour
 {
     [SerializeField] private GameObject _dropItem;
     [SerializeField] private float _yOffset = 0.5f;
+
+    private Transform _spawnRoot;
     private ItemFactory _itemFactory;
 
     private void Awake()
     {
         _itemFactory = ItemFactory.Instance;
+    }
+
+    private void Start()
+    {
+        _spawnRoot = GameObject.FindFirstObjectByType<PoolManager>().transform;
     }
 
     public void DropItem()
@@ -17,8 +24,14 @@ public class MonsterItemDropComponent : MonoBehaviour
         if (ItemFactory.Instance == null) return;
 
         Vector3 offset = new Vector3(0f, _yOffset, 0f);
-        Vector3 spawnPos = transform.position + offset;
+        Vector3 spawnPosition = transform.position + offset;
 
-        _itemFactory.Spawn(_dropItem, spawnPos, Quaternion.identity);
+        GameObject spawned = _itemFactory.Spawn(_dropItem, spawnPosition, Quaternion.identity);
+        if (spawned == null) return;
+
+        if (_spawnRoot != null)
+        {
+            spawned.transform.SetParent(_spawnRoot, true);
+        }
     }
 }
