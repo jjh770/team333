aa030953aab4ad162660a977326e3265cf68f0f7
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-14 13:40:46 +0900

    refactor: 몬스터 전체적인 구조 리팩토링

---
+++ Assets/02.Scripts/Monster/Data/TargetSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum TargetType
+{
+    Player,
+    Flora,
+    Both
+}
+
+public class TargetSelector : MonoBehaviour
+{
+    [Header("Settings")]
+    public TargetType preferredTarget = TargetType.Player;
+
+    private Transform _currentTargetTransform;
+}
