68aa9914fd570613dbfcab911485f7dd7417db65
정종혁 <whdgur0068@gmail.com>
2026-01-15 13:55:58 +0900

    feat : 플로라 스킬 체인저 (토끼) 픽업 시 크기 조절
    픽업 아이템 위치 조절

--- Assets/02.Scripts/Item/Flora/FloraSkillChanger.cs
+++ Assets/02.Scripts/Item/Flora/FloraSkillChanger.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class FloraSkillChanger : ItemBase, IAttractableByFlora
@@ -7,10 +8,30 @@ public class FloraSkillChanger : ItemBase, IAttractableByFlora
 
     private const string FloraTag = "Flora";
 
+    [SerializeField] private float _heldSizeMultifiler = 0.7f;
+    [SerializeField] private float _sizeChangeDuration = 0.5f;
+    [SerializeField] private Ease _sizeChangeEase;
+
+    private Tween _changeSizeTween;
+
+    protected override void PickUp(Transform holder)
+    {
+        base.PickUp(holder);
+        _changeSizeTween?.Kill();
+        _changeSizeTween = transform.DOScale(Vector3.one * _heldSizeMultifiler, _sizeChangeDuration).SetEase(_sizeChangeEase);
+    }
+
+    protected override void Drop()
+    {
+        base.Drop();
+        _changeSizeTween?.Kill();
+        _changeSizeTween = transform.DOScale(Vector3.one, _sizeChangeDuration).SetEase(_sizeChangeEase);
+    }
+
     protected override void OnCollisionEnter(Collision other)
     {
         base.OnCollisionEnter(other);
-        
+
         if (_isHeld) return;
 
         if (!other.gameObject.CompareTag(FloraTag)) return;
