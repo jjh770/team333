0ba670182c4d101cde7e4c260e65f8386ee03aac
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-16 16:31:25 +0900

    chore: 씬 저장

---
+++ Assets/02.Scripts/Item/Consumable/DamageUpItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DamageUpItem : MonoBehaviour, IInteractable
+{
+    [SerializeField] private float _DamageUpAmount;
+
+    public InteractionType Type => InteractionType.Use;
+    public IconType IconType => IconType.Potion;
+
+    public Transform Transform
+    {
+        get
+        {
+            if (this == null) return null;
+            return transform;
+        }
+    }
+    public bool CanInteract => true;
+
+    public void Interact(GameObject interactor)
+    {
+        // 공격력 올려주기
+    }
+}
