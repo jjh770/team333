05aa36300d3639300651c1d220da8a545a7d392a
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-19 15:43:02 +0900

    feat: 마지막에만 엔딩 오브젝트 활성화

--- Assets/02.Scripts/Quest/QuestTree.cs
+++ Assets/02.Scripts/Quest/QuestTree.cs
@@ -5,6 +5,7 @@ public class QuestTree : MonoBehaviour, IDamageable
 {
     [SerializeField] private ConsumableStat _health;
     [SerializeField] private Transform _center;
+    [SerializeField] private GameObject _endingSequence;
 
     public event Action OnTreeDestroyed;
 
@@ -30,6 +31,10 @@ public class QuestTree : MonoBehaviour, IDamageable
         if (_health.IsEmpty)
         {
             OnTreeDestroyed?.Invoke();
+            if (_endingSequence != null)
+            {
+                _endingSequence.SetActive(true);
+            }
             Destroy(gameObject);
         }
         return true;
