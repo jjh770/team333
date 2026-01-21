4f3e565822447eec08e165320fd45e7100d49af8
정종혁 <whdgur0068@gmail.com>
2026-01-21 12:23:25 +0900

    feat : 로비 리더보드 추가

--- Assets/02.Scripts/UI/UI_LobbyMenu.cs
+++ Assets/02.Scripts/UI/UI_LobbyMenu.cs
@@ -8,6 +8,9 @@ public class UI_LobbyMenu : MonoBehaviour
     [SerializeField] private Button _leaderboardButton;
     [SerializeField] private Button _quitButton;
 
+    [Header("Leaderboard")]
+    [SerializeField] private UI_LobbyLeaderboard _lobbyLeaderboard;
+
     private void Start()
     {
         _startButton.onClick.AddListener(OnStartClicked);
@@ -29,8 +32,10 @@ public class UI_LobbyMenu : MonoBehaviour
 
     private void OnLeaderboardClicked()
     {
-        // TODO: 리더보드 패널 구현 예정
-        Debug.Log("Leaderboard - Coming Soon");
+        if (_lobbyLeaderboard != null)
+        {
+            _lobbyLeaderboard.Show();
+        }
     }
 
     private void OnQuitClicked()
