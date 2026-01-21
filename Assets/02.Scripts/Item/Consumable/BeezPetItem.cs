4e4e5db041b7401c2ced4412d03b59c4012ab2eb
정종혁 <whdgur0068@gmail.com>
2026-01-21 15:39:10 +0900

    fix : 벌 아이템 밖으로 카메라 밖으로 못나가게 수정

--- Assets/02.Scripts/Item/Consumable/BeezPetItem.cs
+++ Assets/02.Scripts/Item/Consumable/BeezPetItem.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 
 public class BeezPetItem : ConsumableItemBase
 {
+    [SerializeField] private float _viewportMargin = 0.05f;
+    private Camera _mainCamera;
     public override IconType IconType => IconType.Beez;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        EnforceCameraBounds();
+    }
 
     protected override bool OnConsume(GameObject interactor)
     {
@@ -12,5 +24,10 @@ public class BeezPetItem : ConsumableItemBase
             return true;
         }
         return false;
+    }
+
+    private void EnforceCameraBounds()
+    {
+        CameraBoundsHelper.ClampPositionToCameraBounds(transform, _mainCamera, _viewportMargin);
     }
 }
