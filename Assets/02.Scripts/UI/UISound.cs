6999494e410e5ef8c0b16ce7ec76e3e0a0ade8a7
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-23 10:16:36 +0900

    feat: 호버 사운드 삽입

--- Assets/02.Scripts/UI/UISound.cs
+++ Assets/02.Scripts/UI/UISound.cs
@@ -1,23 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UISound : MonoBehaviour
 {
     [SerializeField] private AudioClip _buttonClickSound;
-    [SerializeField] private float _volumeMultiplier = 1f;
+    [SerializeField] private float _buttonClickSoundVolume = 1f;
+
+    [SerializeField] private AudioClip _mouseHoverSound;
+    [SerializeField] private float _mouseHoverSoundVolume = 0.1f;
 
     private void Awake()
     {
         var buttons = GetComponentsInChildren<Button>(true);
         foreach (var button in buttons)
         {
-            button.onClick.AddListener(PlayButtonSound);
+            button.onClick.AddListener(PlayButtonSound); // 클릭
+            AddHoverSound(button.gameObject); // 호버
         }
     }
 
+    public void AddHoverSound(GameObject target)
+    {
+        var trigger = target.GetComponent<EventTrigger>();
+        if (trigger == null) trigger = target.AddComponent<EventTrigger>();
+        if (trigger.triggers == null) trigger.triggers = new System.Collections.Generic.List<EventTrigger.Entry>();
+
+        var entry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        entry.callback.AddListener(_ => PlayHoverSound());
+        trigger.triggers.Add(entry);
+    }
     public void PlayButtonSound()
     {
         if (_buttonClickSound == null) return;
-        SoundManager.Instance?.PlaySFX(_buttonClickSound, _volumeMultiplier);
+        SoundManager.Instance?.PlaySFX(_buttonClickSound, _buttonClickSoundVolume);
+    }
+
+    public void PlayHoverSound()
+    {
+        if (_mouseHoverSound == null) return;
+        SoundManager.Instance?.PlaySFX(_mouseHoverSound, _mouseHoverSoundVolume);
     }
 }
